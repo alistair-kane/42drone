26c0ced2f39492baf809545e63c5154e03fe687f
Alistair <alkane@student@.42.fr>
2022-08-05 20:29:41 +0200

    new functions

---
+++ gps_send.py
@@ -0,0 +1,59 @@
+from numpy import NaN
+from pymavlink import mavutil
+from marvelmind import MarvelmindHedge
+import sys
+
+def send_gps_data(time, q, x, y, z):
+	"""
+	Updates the drone with Marvelmind external positioning data
+	Args:
+		time_usec (uint64_t): Timestamp (UNIX Epoch time or time since system boot). 
+							The receiving end can infer timestamp format (since 1.1.1970 or since system boot) 
+							by checking for the magnitude of the number.
+		q[4] (float)		: Attitude quaternion (w, x, y, z order, zero-rotation is 1, 0, 0, 0)
+		x (float)			: X position (NED)
+		y (float)			: Y position (NED)
+		z (float)			: Z position (NED)
+
+	convariance:
+	Row-major representation of a pose 6x6 cross-covariance matrix upper right triangle 
+	(states: x, y, z, roll, pitch, yaw; first six entries are the first ROW, next five 
+	entries are the second ROW, etc.). If unknown, assign NaN value to first element in the array.
+	"""
+	master.mav.att_pos_mocap_send(
+		time,
+		q,
+		x,
+		y,
+		z,
+		NaN)
+
+#enter address value for each drone here?
+# hedge = MarvelmindHedge(tty = "/dev/ttyUSB1", adr=None, debug=False)
+# hedge.start()
+
+master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600)
+master.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component))
+
+
+while True:
+	msg = master.recv_match()
+	if not msg:
+		continue
+	if msg.get_type() == 'HEARTBEAT':
+		print("\n\n*****Got message: %s*****" % msg.get_type())
+		print("Message: %s" % msg)
+		print("\nAs dictionary: %s" % msg.to_dict())
+		# Armed = MAV_STATE_STANDBY (4), Disarmed = MAV_STATE_ACTIVE (3)
+		print("\nSystem status: %s" % msg.system_status)
+
+# while True:
+# 	try:
+# 		hedge.dataEvent.wait(1)
+# 		hedge.dataEvent.clear()
+# 		if (hedge.fusionImuUpdated)
+# 			hedge.print_imu_fusion()
+# 	except KeyboardInterrupt:
+# 		hedge.stop()
+# 		sys.exit()
--- test_functions.py
+++ test_functions.py
@@ -1,82 +1,117 @@
-from numpy import NaN
+from statistics import covariance
+from numpy import NaN, uint64
 from pymavlink import mavutil
 from marvelmind import MarvelmindHedge
 import sys
+import time
+import numpy as np
 
-def request_message_interval(message_id: int, frequency_hz: float):
-    """
-    Request MAVLink message in a desired frequency,
-    documentation for SET_MESSAGE_INTERVAL:
-        https://mavlink.io/en/messages/common.html#MAV_CMD_SET_MESSAGE_INTERVAL
-
-    Args:
-        message_id (int): MAVLink message ID
-        frequency_hz (float): Desired frequency in Hz
-    """
-    master.mav.command_long_send(
-        master.target_system, master.target_component,
-        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
-        message_id, # The MAVLink message ID
-        1e6 / frequency_hz, # The interval between two messages in microseconds. Set to -1 to disable and 0 to request default rate.
-        0, 0, 0, 0, # Unused parameters
-        0, # Target address of message stream (if message has target address fields). 0: Flight-stack default (recommended), 1: address of requestor, 2: broadcast.
-    )
-
-def send_gps_data(time, q, x, y, z):
+def request_message_interval(message_input: str, frequency_hz: float):
 	"""
-	Updates the drone with Marvelmind external positioning data
+	Request MAVLink message in a desired frequency,
+	documentation for SET_MESSAGE_INTERVAL:
+		https://mavlink.io/en/messages/common.html#MAV_CMD_SET_MESSAGE_INTERVAL
+
 	Args:
-		time_usec (uint64_t): Timestamp (UNIX Epoch time or time since system boot). 
-							The receiving end can infer timestamp format (since 1.1.1970 or since system boot) 
-							by checking for the magnitude of the number.
-		q[4] (float)		: Attitude quaternion (w, x, y, z order, zero-rotation is 1, 0, 0, 0)
-		x (float)			: X position (NED)
-		y (float)			: Y position (NED)
-		z (float)			: Z position (NED)
-
-	convariance:
-	Row-major representation of a pose 6x6 cross-covariance matrix upper right triangle 
-	(states: x, y, z, roll, pitch, yaw; first six entries are the first ROW, next five 
-	entries are the second ROW, etc.). If unknown, assign NaN value to first element in the array.
+		message_id (str): MAVLink message ID
+		frequency_hz (float): Desired frequency in Hz
 	"""
-	master.mav.att_pos_mocap_send(
-		time,
-		q,
-		x,
-		y,
-		z,
-		NaN)
-
-#can filter the beacon address here?
-hedge = MarvelMindHedge(tty = "/dev/ttyUSB1", adr=None, debug=False)
-hedge.start()
-
-master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600)
-master.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component))
-
-request_message_interval(mavutil.mavlink.AUTOPILOT_VERSION, 10)
-
-while True:
-	msg = master.recv_match()
-	if not msg:
-		continue
-	if msg.get_type() == 'HEARTBEAT':
-		print("\n\n*****Got message: %s*****" % msg.get_type())
-		print("Message: %s" % msg)
-		print("\nAs dictionary: %s" % msg.to_dict())
-		# Armed = MAV_STATE_STANDBY (4), Disarmed = MAV_STATE_ACTIVE (3)
-		print("\nSystem status: %s" % msg.system_status)
+	message_name = "MAVLINK_MSG_ID_" + message_input
+	message_id = getattr(mavutil.mavlink, message_name)
+	master.mav.command_long_send(
+		master.target_system, master.target_component,
+		mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
+		message_id,
+		1e6 / frequency_hz,
+		0,
+		0, 0, 0, 0)
+	print("Requested the message successfully.")
 
 
 
+def get_requested_data(message_name: str, dict_key: str, value_unit: str, save_name: str):
+	try:
+		message_index = 0
+		dict1 = master.recv_match(type= message_name, blocking=True, timeout=0.1).to_dict()
+		dict_value = dict1[dict_key]
 
-# while True:
-# 	try:
-# 		hedge.dataEvent.wait(1)
-# 		hedge.dataEvent.clear()
-# 		if (hedge.fusionImuUpdated)
-# 			hedge.print_imu_fusion()
-# 	except KeyboardInterrupt:
-# 		hedge.stop()
-# 		sys.exit()
+		toWrite = "Message_Index, " + message_index + " :" + str(dict_value) + value_unit
+		with open(save_name, 'a') as file:
+			file.write(toWrite)
+			file.write('\n')  
+			message_index += 1
+	except:
+		pass
+
+
+# https://mavlink.io/en/messages/common.html#VISION_POSITION_ESTIMATE
+def send_vision_position_estimate_message(x: float, y: float z: float):
+	global current_time_us, H_aeroRef_aeroBody, reset_counter
+	with lock:
+		covariance = np.zeros(21, dtype=float)
+		# Send the message
+		master.mav.vision_position_estimate_send(
+			current_time_us,            # us Timestamp (UNIX time or time since system boot)
+			x,   # Global X position
+			y,   # Global Y position
+			z,   # Global Z position
+			13,	                # Roll angle
+			13,	                # Pitch angle
+			13,	                # Yaw angle
+			covariance,         # Row-major representation of pose 6x6 cross-covariance matrix
+			0,               	# Estimate reset counter. Increment every time pose estimate jumps.
+		)
+
+def main():
+
+	#can filter the beacon address here?
+	hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=None, debug=False)
+	hedge.start()
+
+	global master 
+	master = mavutil.mavlink_connection("/dev/ttyUSB1", baud=57600)
+	master.wait_heartbeat()
+	print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component))
+
+	
+
+	request_message_interval("GPS_RAW_INT", 1.0)
+	request_message_interval("VFR_HUD", 1.0)
+
+	while True:
+		try:
+			hedge.dataEvent.wait(1)
+			hedge.dataEvent.clear()
+
+			if (hedge.positionUpdated):
+				# hedge.print_position()
+				data = hedge.position()
+				# print(data)
+				print("time: %f x: %f y: %f z: %f" % (data[5], data[1], data[2], data[3]))
+				send_vision_position_estimate_message(data[1], data[2], data[3])
+		except KeyboardInterrupt:
+			hedge.stop()
+			sys.exit()
+
+		get_requested_data('GPS_RAW_INT', 'fix_type', ' ', 'gps log')
+		get_requested_data('VFR_HUD', 'alt', 'm', 'vfr_log')
+
+		time.sleep(0.1)
+
+main()
+
+
+
+	# request_message_interval(mavutil.mavlink.AUTOPILOT_VERSION_REQUEST, 50)
+	# request_message_interval(148, 100)
+
+
+	# while True:
+	# 	msg = master.recv_match()
+	# 	if not msg:
+	# 		continue
+	# 	if msg.get_type() == 'AUTOPILOT_VERSION':
+	# 		print("\n\n*****Got message: %s*****" % msg.get_type())
+	# 		print("Message: %s" % msg)
+	# 		print("\nAs dictionary: %s" % msg.to_dict())
+	# 	time.sleep(0.1)
