bee414c3c60ca5bfa40a3b974cae3e0a382cc160
alkane <alistair@student.42.fr>
2022-08-08 18:46:39 +0200

    test arm code

--- test.py
+++ test.py
@@ -3,45 +3,23 @@ from marvelmind import MarvelMindHedge
 import time
 import sys
 
-hedge = MarvelMindHedge(tty = "/dev/ttyUSB1", adr=None, debug=False)
-hedge.start()
+# hedge = MarvelMindHedge(tty = "/dev/ttyUSB1", adr=None, debug=False)
+# hedge.start()
 
-master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600)
+master = mavutil.mavlink_connection("udpin:127.0.0.1:15667", baud=57600)
 
-while True:
-	
+
 master.wait_heartbeat()
-
 print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component))
-# msg = master.recv_match(type='SYS_STATUS', blocking=True)
 
-# msg = master.recv_match(type='GPS_RAW_INT')
 
-# master.mav.gps_input_send()
+master.mav.command_long_send(master.target_system, master.target_component,
+                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+msg = master.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
 
-# Arm
-# master.arducopter_arm()
 
-# master.mav.command_long_send(
-#     master.target_system,
-#     master.target_component,
-#     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-#     0,
-#     1, 0, 0, 0, 0, 0, 0)
-
-# wait until arming confirmed (can manually check with master.motors_armed())
-# print("Waiting for the vehicle to arm")
-# master.motors_armed_wait()
-# print('Armed!')
-
-# Disarm
-# master.arducopter_disarm() or:
-# master.mav.command_long_send(
-	# master.target_system,
-	 # master.target_component,
-#     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-#     0,
-#     0, 0, 0, 0, 0, 0, 0)
-
-# wait until disarming confirmed
-# master.motors_disarmed_wait()
+master.mav.command_long_send(master.target_system, master.target_component,
+                                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
+msg = master.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
